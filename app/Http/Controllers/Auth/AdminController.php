5bd3164070b6099f56e9069200d98968776cc35f
Chukwuemeka Okoroafor <emeka.devsq@olotusquare.co>
2018-08-22 18:00:57 +0100

    AdminLogin Controller setup

---
+++ app/Http/Controllers/Auth/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+    /**
+     * show dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "admin dashboard";
+//        return view('admin.dashboard');
+    }
+}
