89447691c84a4e3b45f92843a8b396ea9c7ec7c7
Chukwuemeka Okoroafor <emeka.devsq@olotusquare.co>
2018-08-22 17:52:35 +0100

    Added Admin Model and Migration File

---
+++ app/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Authenticatable
+{
+    protected $guard = 'admin';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
